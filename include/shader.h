24b64e7c7a2da19bacc8182032629da42299a463
Christian Ermann <christianermann@gmail.com>
2021-11-17 22:25:06 -0800

    Add shader header file

---
+++ include/shader.h
@@ -0,0 +1,20 @@
+#ifndef SHADER_H
+#define SHADER_H
+
+#include "vec.h"
+
+#include "glad/glad.h"
+
+typedef struct Shader Shader;
+
+Shader *Shader_make(const char *vert_filename, const char *frag_filename);
+void Shader_free(Shader *shader);
+
+void Shader_use(Shader *shader);
+
+void Shader_reload(Shader *shader);
+
+void Shader_setVec3(const Shader *shader, const char *name, Vec3 v);
+void Shader_setMat4(const Shader *shader, const char *name, Mat4 m);
+
+#endif
