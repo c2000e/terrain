4c1d97d987c0ab5942fa0f0c53fddfb45e299e3c
Christian Ermann <christianermann@gmail.com>
2022-07-29 18:57:19 -0700

    Added set-width type definitions

---
+++ include/types.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <stdint.h>
+
+// Unsigned integer types
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+// Signed integer types
+typedef int8_t  i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+// Floating point types
+typedef float  f32;
+typedef double f64;
+
+// Boolean types
+typedef int8_t b8;
+
+#define true  1
+#define false 0
+
